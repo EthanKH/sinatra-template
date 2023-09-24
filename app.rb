273ca7aec11ba70399fc5a5a8176d0ef04ac32e3
Ethan Huang <96758695+EthanKH@users.noreply.github.com>
2023-09-24 16:18:58 +0000

    more edits

--- app.rb
+++ app.rb
@@ -7,7 +7,6 @@ require 'erb'
 
 require "better_errors"
 require "binding_of_caller"
-
 use(BetterErrors::Middleware)
 BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
@@ -61,12 +60,36 @@ get("/game") do
   image_url = generate_animal
   image_url2 = generate_animal2
   erb :game, locals: { image_url: image_url, image_url2: image_url2 }
+
+  # <form action="/win_lose" method="post">
+  #       <button type="submit">Pick me!:)</button>
+  #     </form>
+
+  # <form action="/win_lose" method="get">
+  #   <button type="submit">I'm done choosing</button>
+  # </form>
 end
 
 get("/random") do
   image_url = generate_animal
   erb :random, locals: { image_url: image_url }
 end
+
+get '/pick_winner/:winner' do
+  winner = params['winner'].to_i
+
+  # Determine which button was clicked by checking the 'winner' param
+  if winner == 1
+    @winners << @image_url1
+    @losers << @image_url2
+  elsif winner == 2
+    @winners << @image_url2
+    @losers << @image_url1
+  end
+
+  redirect '/game' # Redirect back to the game page
+end
+
 
 get("/win_lose") do
   erb (:win_lose)
